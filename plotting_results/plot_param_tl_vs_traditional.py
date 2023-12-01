7acc2723f2f12cd35f949ca2bda7236c5391c60d
DuneeshaFernando <duneesha18@gmail.com>
2023-12-01 15:25:38 +1100

    Update plotting code

--- plotting_results/plot_param_tl_vs_traditional.py
+++ plotting_results/plot_param_tl_vs_traditional.py
@@ -1,10 +1,11 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+from matplotlib.ticker import FormatStrFormatter
 
 traditional_path = 'param_tl_results/traditional/traditional_test_loss_m311.csv'
 traditional_df = pd.read_csv(traditional_path, index_col=0, header=None)
 
-param_tl_path = 'param_tl_results/param_tl/param_tl_test_loss_m311_final_draft.csv'
+param_tl_path = 'param_tl_results/param_tl/param_tl_test_loss_m311.csv'
 param_tl_df = pd.read_csv(param_tl_path, index_col=0, header=None)
 
 # Append 50-len(no.of lines in param_tl_df) to param_tl_df
@@ -18,12 +19,15 @@ ax = plt.subplot(1,1,1)
 ax.plot(traditional_df, marker='.')
 ax.plot(param_tl_df[:param_tl_df_init_len], marker='.', color='orange')
 ax.plot(param_tl_df[param_tl_df_init_len:], color='orange', linestyle = '--')
-
-ax.set_title("Machine-3-11")
+plt.rcParams.update({'font.size': 15})
+ax.set_title("Device-3-11")
 ax.set_xlim(-1,50)
+ax.yaxis.set_major_formatter(FormatStrFormatter('%.2f'))
 # ax.set_ylim(0.004,0.018)
-ax.set_xlabel('No.of epochs')
-ax.set_ylabel('Test loss')
-ax.legend(['Traditional training', 'Similarity-based Parameter Transfer Learning'])
+ax.set_xlabel('No.of epochs', fontsize = 16)
+ax.set_ylabel('Test loss', fontsize = 16)
+ax.legend(['Traditional training', 'Intra-cluster Parameter\n Transfer Learning'])
+plt.xticks(fontsize = 13)
+plt.yticks(fontsize = 13)
 # plt.show()
 plt.savefig('final_plots/m34tom311.png')
